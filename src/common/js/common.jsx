d96cadf68cce56f519378f52b7cdeb8770cdbdf4
DESKTOP-LT1NPT5\samsung <baekjw@uracle.co.kr>
2025-11-13 08:32:12 +0900

    change 20251113

--- src/common/js/common.jsx
+++ src/common/js/common.jsx
@@ -1,4 +1,4 @@
-const apiBaseUrl = 'https://skin-area-increasingly-tabs.trycloudflare.com' + '/api';
+const apiBaseUrl = 'https://quantitative-commitment-qualified-stayed.trycloudflare.com' + '/api';
 
 import React from "react";
 import ReactDOM from "react-dom/client";
