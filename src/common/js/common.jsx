faaf11098f3224d21390da7492204f8fe4c5b7a9
DESKTOP-LT1NPT5\samsung <baekjw@uracle.co.kr>
2025-12-26 15:04:30 +0900

    fasdf

--- src/common/js/common.jsx
+++ src/common/js/common.jsx
@@ -1,4 +1,4 @@
-const apiBaseUrl = 'https://grateful-racks-orders-relate.trycloudflare.com' + '/api';
+const apiBaseUrl = 'https://immunology-gentleman-investigate-results.trycloudflare.com' + '/api';
 
 import React from "react";
 import ReactDOM from "react-dom/client";
