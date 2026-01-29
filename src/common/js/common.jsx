f8d3fc9a90679e9a35cd0fc98252c63c5789194a
DESKTOP-LT1NPT5\samsung <baekjw@uracle.co.kr>
2026-01-29 13:17:29 +0900

    ddddd

--- src/common/js/common.jsx
+++ src/common/js/common.jsx
@@ -1,4 +1,4 @@
-const apiBaseUrl = 'https://vocal-confidence-grande-desired.trycloudflare.com' + '/api';
+const apiBaseUrl = 'https://sheriff-coated-videos-ports.trycloudflare.com' + '/api';
 
 import React from "react";
 import ReactDOM from "react-dom/client";
