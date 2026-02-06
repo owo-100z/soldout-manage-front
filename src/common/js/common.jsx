a6969e4e3a306a3babdbd433f049d3d0360129da
100zowo <100zowo@gmail.com>
2026-02-06 15:31:59 +0900

    fasdfasdf

--- src/common/js/common.jsx
+++ src/common/js/common.jsx
@@ -1,4 +1,4 @@
-const apiBaseUrl = 'https://genres-stuart-foundation-birmingham.trycloudflare.com' + '/api';
+const apiBaseUrl = 'https://atlanta-myers-pet-recorded.trycloudflare.com' + '/api';
 
 import React from "react";
 import ReactDOM from "react-dom/client";
