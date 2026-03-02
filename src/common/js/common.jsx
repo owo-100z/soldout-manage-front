210a0daa1115747fb37a19d0b58c3e11710b64d8
100zowo <100zowo@gmail.com>
2026-03-03 08:44:09 +0900

    asgas

--- src/common/js/common.jsx
+++ src/common/js/common.jsx
@@ -1,4 +1,4 @@
-const apiBaseUrl = 'https://socks-anyone-boards-upc.trycloudflare.com' + '/api';
+const apiBaseUrl = 'https://technique-const-representative-sunday.trycloudflare.com' + '/api';
 
 import React from "react";
 import ReactDOM from "react-dom/client";
